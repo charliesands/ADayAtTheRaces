cd6afdd932aad2907c491e6e7526e18eabd39c19
charliesands <cwsands3@gmail.com>
2018-11-20 16:11:02 -0700

    bet class setup

--- ADayAtTheRaces/Bet.cs
+++ ADayAtTheRaces/Bet.cs
@@ -14,12 +14,26 @@ namespace ADayAtTheRaces
 
         public string GetDescription()
         {
-
+            if (Amount > 0)
+            {
+                return Bettor.Name + " bets " + Amount + " on dog #" + Dog + ".";
+            }
+            else
+            {
+                return Bettor.Name + " has not placed a bet.";
+            }
         }
 
         public int PayOut(int Winner)
         {
-
+            if (Winner == Dog)
+            {
+                return Amount;
+            }
+            else
+            {
+                return -Amount;
+            }
         }
     }
 }
