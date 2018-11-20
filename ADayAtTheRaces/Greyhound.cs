00c1952bb5b9f975c5b157bb2dd934eea899d7a2
charliesands <cwsands3@gmail.com>
2018-11-19 17:36:46 -0700

    form and class setup

---
+++ ADayAtTheRaces/Greyhound.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ADayAtTheRaces
+{
+    public class Greyhound
+    {
+        public int StartingPosition;
+        public int RacetrackLength;
+        public PictureBox MyPictureBox = null;
+        public int Location = 0;
+        public Random Randomizer;
+
+        public bool Run()
+        {
+
+        }
+
+    }
+}
